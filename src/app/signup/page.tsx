4a6d2289af91cd66e7f2fef3653183f78b028ba0
KURUMI <k.1au12um1@gmail.com>
2024-10-20 11:31:21 +0900

    サインアップ画面作成

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
 import React, { useContext } from "react";
 
-const Signin=()=>{
+const Signup=()=>{
 
     return (
         <div className="max-w-md mx-auto p-6 border border-gray-300 rounded-lg bg-gray-100">
             <h1 className="text-center text-2xl font-bold mb-4">
-                サインイン
+                新規登録
                 </h1>
             <form>
+            <label className="block mb-2">
+                    My Name
+                    <input name="myname" 
+                    type="text" 
+                    placeholder="nameを入力してください"
+                    className="mt-1 block w-full p-2 border border-gray-300 rounded"
+                    />
+                </label>
                 <label className="block mb-2">
                     Emaill
                     <input name="email" 
                     type="email" 
-                    placeholder="emailを入力"
+                    placeholder="emailを入力してください"
                     className="mt-1 block w-full p-2 border border-gray-300 rounded"
                     />
                 </label>
@@ -21,23 +29,23 @@ const Signin=()=>{
                     Password
                     <input name="password" 
                     type="password" 
-                    placeholder="passwordを入力"
+                    placeholder="passwordを入力してください"
                     className="mt-1 block w-full p-2 border border-gray-300 rounded"
                     />
                 </label>
                 <button type="submit"
                 className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                    サインイン
+                    新規登録
                     </button>
             </form>
             <div>
                 <h2>
-                    新規登録は
-                    <Link href={"/signup"}>こちら→</Link>
+                    サインインは
+                    <Link href={"/signin"}>こちら→</Link>
                 </h2>
             </div>
         </div>
     );
 };
 
-export default Signin;
+export default Signup;
